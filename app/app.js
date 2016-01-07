c02c53f1e794f846ae6641f5a1a6b7f244b7ec46
bryan kennedy <bkennedy@smm.org>
2016-01-07 15:29:20 -0600

    Adding external vendor dependencies

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
-// Here is the starting point for your application code.
-// All stuff below is just to show you how it works. You can delete all of it.
-
-// Use new ES6 modules syntax for everything.
 import os from 'os'; // native node.js module
 import { remote } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
 import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
+
+
+import { jquery } from './vendor/jquery/dist/jquery.min.js';
+import { tween } from './vendor/gsap/src/minified/TweenMax.min';
+import { Zoomer } from './vendor/Zoomer/jquery.fs.zoomer.min';
+import { videojs } from './vendor/video-js/dist/video-js/video';
 
 console.log('Loaded environment variables:', env);
 
