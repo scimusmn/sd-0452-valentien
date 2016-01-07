46274730f7dc1e7aff974505a82d1b13cf21a6d1
bryan kennedy <bkennedy@smm.org>
2016-01-07 15:31:14 -0600

    Removing boilerplate junk

--- app/app.js
+++ app/app.js
@@ -15,10 +15,6 @@ console.log('Loaded environment variables:', env);
 var app = remote.app;
 var appDir = jetpack.cwd(app.getAppPath());
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files like it is node.js! Welcome to Electron world :)
-console.log('The author of this app is:', appDir.read('package.json', 'json').author);
-
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('greet').innerHTML = greet();
   document.getElementById('platform-info').innerHTML = os.platform();
