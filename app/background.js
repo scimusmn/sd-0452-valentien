22cace047b67555bd6b3e85990d27b052f2d0a45
bryan kennedy <bkennedy@smm.org>
2016-01-11 13:27:10 -0600

    Enable kiosk on production build only
    This makes it easier for development when just doing development.

--- app/background.js
+++ app/background.js
@@ -17,21 +17,30 @@ app.on('ready', function() {
   var mainWindow = new BrowserWindow({
     x: 0,
     y: 0,
-    width: 1080,
-    height: 1920,
+    fullscreen: true,
   });
 
   /**
-   * Hack to make fullscreen kiosk actually work.
+   * Hack to make fullscreen kiosk mode actually work.
    *
    * There is an active bug with Electron, kiosk mode, and Yosemite.
    * https://github.com/atom/electron/issues/1054
    * This hack makes kiosk mode actually work by waiting for the app to launch
    * and then issuing a call to go into kiosk mode after a few milliseconds.
    */
-  setTimeout(function() {
-    mainWindow.setKiosk(true);
-  }, 100);
+  if (env.name == 'production') {
+    setTimeout(function() {
+      mainWindow.setKiosk(true);
+    }, 100);
+  }
+
+  /**
+   * Show dev tools when we're not in production mode
+   */
+  if (env.name !== 'production') {
+    devHelper.setDevMenu();
+    mainWindow.openDevTools();
+  }
 
   if (env.name === 'test') {
     mainWindow.loadURL('file://' + __dirname + '/spec.html');
